3a46685ddf3542a67dd00aac19a2e99ad3e64e64
ollykr <okremianski@gmail.com>
2022-07-03 19:29:05 -0400

    Pre-cache assets

--- sw.js
+++ sw.js
@@ -1,17 +1,40 @@
+const staticCacheName = "site-static";
+// store requests urls users can make
+const assets = [
+	"/",
+	"/index.html",
+	"/js/app.js",
+	"/js/ui.js",
+	"/js/materialize.min.js",
+	"/css/styles.css",
+	"/css/materialize.min.css",
+	"/img/dish.png",
+	"https://fonts.googleapis.com/icon?family=Material+Icons",
+	"https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
 // install service worker
 // "self" refers to the service worker itself
 // later on this is where we cache static assets that don't change much/often, e.g css and images
 self.addEventListener("install", (evt) => {
-	console.log("service worker has been installed");
+	// console.log("service worker has been installed");
+	// Reaching out to cache API
+	// it opens a "staticCacheName" cache and use it, if it exists, otherwise it creates it
+	// this is async task
+	evt.waitUntill(
+		caches.open(staticCacheName).then((cache) => {
+			console.log("caching shell assets");
+			cache.addAll(assets);
+		})
+	);
 });
 
 // activate service worker/listening to activate event
 // automatically becomes activated
 self.addEventListener("activate", (evt) => {
-	console.log("Service Worker has been activated");
+	// console.log("Service Worker has been activated");
 });
 // fetch event
 // we update the code so SW is re-installed but not activated , waiting to be activated
 self.addEventListener("fetch", (evt) => {
-	console.log("fetch event", evt);
+	// console.log("fetch event", evt);
 });
